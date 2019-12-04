7f7c20164d554f3780099e97862a1b3c565e3d8c
rinse <rinse@neko2.net>
2019-12-04 12:47:14 +0900

    add my xmonad.hs

---
+++ lib/Lib.hs
@@ -0,0 +1,18 @@
+module Lib
+    ( lock
+    , incVolume
+    , decVolume
+    , toggleMute
+    ) where
+
+import           Control.Monad.IO.Class (MonadIO)
+import           XMonad.Util.Run        (safeSpawn, safeSpawnProg)
+
+
+lock :: MonadIO m => m ()
+lock = safeSpawnProg "lxlock"
+
+incVolume, decVolume, toggleMute :: MonadIO m => m ()
+incVolume = safeSpawn "pactl" [ "set-sink-volume", "@DEFAULT_SINK@", "+10%" ]
+decVolume = safeSpawn "pactl" [ "set-sink-volume", "@DEFAULT_SINK@", "-10%" ]
+toggleMute = safeSpawn "pactl" [ "set-sink-mute", "@DEFAULT_SINK@", "toggle" ]
--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,79 @@
 module Main where
 
-import XMonad
+{-
+    This is xmonad.hs for me.
+    You may need to install the following:
+      - dmenu, xmobar
+    If you don't have, the following as well:
+      - pactl
+    Troubleshootings:
+      Some programs open a weird file manager
+        $ xdg-mime default pcmanfm.desktop inode/directory
+      Java programs don't work very well
+        $ export _JAVA_AWT_WM_NONREPARENTING=1
+-}
+
+import           Data.Ratio                  ((%))
+import           Lib                         (decVolume, incVolume, lock,
+                                              toggleMute)
+import           XMonad
+import           XMonad.Actions.CycleWS      (nextScreen, shiftNextScreen)
+import           XMonad.Actions.Minimize     (maximizeWindow, minimizeWindow,
+                                              withLastMinimized)
+import           XMonad.Hooks.DynamicLog     (xmobar)
+import           XMonad.Hooks.EwmhDesktops   (ewmh)
+import           XMonad.Hooks.ManageDocks    (avoidStruts, docks)
+import           XMonad.Layout.BoringWindows (boringWindows, focusDown, focusUp)
+import           XMonad.Layout.Minimize      (minimize)
+import           XMonad.Layout.NoBorders     (noBorders)
+import           XMonad.Layout.ResizableTile (MirrorResize (MirrorExpand, MirrorShrink),
+                                              ResizableTall (ResizableTall))
+import qualified XMonad.StackSet             as W
+import           XMonad.Util.EZConfig        (additionalKeysP, removeKeysP)
+import           XMonad.Util.Run             (safeSpawnProg)
 
 
 main :: IO ()
-main = def >>= xmonad
+main = xmobar myConfig >>= xmonad
+    where
+    myConfig = docks . ewmh . configKeys $ def
+        { borderWidth = 3 :: Dimension
+        , focusFollowsMouse = False
+        , layoutHook = myLayout
+        , modMask = mod1Mask :: KeyMask
+        , terminal = myTerminal
+        }
+    myLayout = avoidStruts . boringWindows . minimize $ myTiles
+        where
+        myTiles = myTile ||| Mirror myTile ||| noBorders Full
+        myTile = ResizableTall 1 (3 % 100) (1 / phi) [1, 6 % 5]
+        phi = 8 % 5
+
+myTerminal :: String
+myTerminal = "lxterminal"
+
+configKeys :: XConfig l -> XConfig l
+configKeys c = c `additionalKeysP` myAdditionalKeys `removeKeysP` myRemovedKeys
+    where
+    myAdditionalKeys :: [(String, X ())]
+    myAdditionalKeys =
+        [ ("M4-l", lock)
+        , ("M1-C-t", safeSpawnProg myTerminal)
+        , ("M-a", sendMessage MirrorShrink)
+        , ("M-z", sendMessage MirrorExpand)
+        , ("M-j", focusDown)
+        , ("M-k", focusUp)
+        , ("M-r", nextScreen)
+        , ("M-S-r", shiftNextScreen >> nextScreen)
+        , ("M-i", windows W.swapMaster)
+        , ("M-m", withFocused minimizeWindow)
+        , ("M-S-m", withLastMinimized maximizeWindow)
+        , ("M-u a", incVolume)
+        , ("M-u x", decVolume)
+        , ("M-u m", toggleMute)
+        ]
+    myRemovedKeys :: [String]
+    myRemovedKeys =
+        [ "M-S-q"
+        , "M1-<Return>" -- For intellij idea. Use M-i instead
+        ]
